804b563bacf9ae88fa30013ae70df31333f72d5f
sycasec <sycasec@arch.btw>
2023-08-06 15:29:14 +0800

    created logging branch, changes are just curly braces

--- PlayerServices/PlayerService.cs
+++ PlayerServices/PlayerService.cs
@@ -1,27 +1,45 @@
 global using typeRacingAPI.Models;
 
-namespace typeRacingAPI.PlayerServices {
-    public class PlayerService : IPlayerService {
+namespace typeRacingAPI.PlayerServices
+{
+    public class PlayerService : IPlayerService
+    {
 
-        private static List<Player> tempPlayerList = new List<Player>() {
-            new Player { ID = 1, Name = "Kyle", Accuracy = 100.00f, Score = 100.00f, Timestamp = DateTime.Now},
-            new Player { ID = 2, Name = "Not Kyle", Accuracy = 99.98f, Score = 69.96f, Timestamp = DateTime.Now.AddMinutes(-60)},
-        };
         private readonly DataContext _context;
-        public PlayerService(DataContext context){
+        public PlayerService(DataContext context)
+        {
             _context = context;
         }
 
-        //TODO
-        public async Task<SvcResponse<string>> AddPlayer(Player p) {
+        private int GenerateNewID()
+        {
+            // You can implement your own logic to generate a unique player ID
+            // For example, you can use int.Newint().ToString() to generate a unique ID
+            int lastPlayerID = 0;
+
+            if (_context.Players.Any())
+            {
+                lastPlayerID = _context.Players.Max(p => p.ID);
+            }
+
+            int newID = lastPlayerID + 1;
+            return newID;
+        }
+
+        public async Task<SvcResponse<string>> AddPlayer(Player p)
+        {
             var res = new SvcResponse<string>();
             var localPlayer = await _context.Players.FindAsync(p.ID);
-            if (localPlayer != null) {
+            if (localPlayer != null)
+            {
                 res.Data = "Task Failed.";
                 res.Success = false;
                 res.Message = "Cannot reinsert an existing player with ID";
-            } else {
+            }
+            else
+            {
                 p.Timestamp = DateTime.UtcNow;
+                p.ID = GenerateNewID();
                 _context.Players.Add(p);
                 await _context.SaveChangesAsync();
                 res.Data = "Task Success.";
@@ -30,37 +48,47 @@ namespace typeRacingAPI.PlayerServices {
             return res;
         }
 
-        public async Task<SvcResponse<IEnumerable<Player>>> GetAllPlayers() {
+        public async Task<SvcResponse<IEnumerable<Player>>> GetAllPlayers()
+        {
             var dbPlayers = await _context.Players.ToListAsync();
-            var res = new SvcResponse<IEnumerable<Player>> {
+            var res = new SvcResponse<IEnumerable<Player>>
+            {
                 Data = dbPlayers,
                 Message = "200",
             };
             return res;
         }
 
-        public async Task<SvcResponse<Player?>> GetPlayerByID(int player_id) {
+        public async Task<SvcResponse<Player?>> GetPlayerByID(int player_id)
+        {
             var player = await _context.Players.FindAsync(player_id);
             var res = new SvcResponse<Player?>();
-            if (player == null) {
+            if (player == null)
+            {
                 res.Data = player;
                 res.Message = $"Player with ID={player_id} does not exist!";
                 res.Success = false;
-            } else {
+            }
+            else
+            {
                 res.Data = player;
                 res.Message = "200";
             }
             return res;
         }
 
-        public async Task<SvcResponse<string>> UpdatePlayerByID(int player_id, Player pReq) {
+        public async Task<SvcResponse<string>> UpdatePlayerByID(int player_id, Player pReq)
+        {
             var player = await _context.Players.FindAsync(player_id);
             var res = new SvcResponse<string>();
-            if (player == null) {
+            if (player == null)
+            {
                 res.Data = "Task Failed.";
                 res.Success = false;
                 res.Message = $"Player with ID={player_id} does not exist";
-            } else {
+            }
+            else
+            {
                 player.Name = pReq.Name;
                 player.Accuracy = pReq.Accuracy;
                 player.Score = pReq.Score;
@@ -72,14 +100,18 @@ namespace typeRacingAPI.PlayerServices {
             return res;
         }
 
-        public async Task<SvcResponse<string>> DeletePlayer(int player_id) {
-            var player = tempPlayerList.Find(p => p.ID == player_id);
+        public async Task<SvcResponse<string>> DeletePlayer(int player_id)
+        {
+            var player = await _context.Players.FindAsync(player_id);
             var res = new SvcResponse<string>();
-            if (player is null) {
+            if (player is null)
+            {
                 res.Success = false;
                 res.Message = $"Player with ID={player_id} does not exist";
                 res.Data = "Task Failed.";
-            } else {
+            }
+            else
+            {
                 _context.Players.Remove(player);
                 res.Data = "Task Success.";
                 await _context.SaveChangesAsync();
